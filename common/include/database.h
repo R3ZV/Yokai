61558707e7ee1fc00bf4846d74825d1e797612cc
R3ZV <razvan.maracine@protonmail.com>
2025-05-01 20:29:41 +0300

    Formatt & cleanup

--- common/include/database.h
+++ common/include/database.h
@@ -1,9 +1,12 @@
 #pragma once
+
 #include "object.h"
-#include<map>
-#include<string>
+#include <map>
+#include <string>
+
 class Database{
     std::map<std::string, Object> content;
+
 public:
     void insert(std::string key, Object value);
     Object get(std::string key);
--- common/include/object.h
+++ common/include/object.h
@@ -3,10 +3,13 @@
 #include<variant>
 #include<string>
 #include<optional>
+
 class Object{
-    std::variant<int, std::string> data; //for mow, we support ints and strings as values
+    //for mow, we support ints and strings as values
     //TODO: add support for fancier types, such as JSON
-    public:
+    std::variant<int, std::string> data;
+
+public:
     Object(int integer_data);
     Object(const std::string &str_data);
     std::optional<int> asInt() const;

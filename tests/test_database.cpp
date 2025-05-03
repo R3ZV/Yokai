a1a8f7c865a20bd8ba75a56d9549dd81a10159fe
R3ZV <razvan.maracine@protonmail.com>
2025-05-03 20:24:00 +0300

    fixed format & new test

--- tests/test_database.cpp
+++ tests/test_database.cpp
@@ -1,5 +1,5 @@
-#include "../lib/doctest.h"
 #include "../daemon/include/database.h"
+#include "../lib/doctest.h"
 
 TEST_CASE("Testing Database insert_key") {
     Database* database = new Database();
@@ -14,6 +14,19 @@ TEST_CASE("Testing Database insert_key") {
     CHECK(data.find("test_2") != data.end());
     CHECK(data.find("test_2")->second.get()->asString() == "radu");
 }
+
+TEST_CASE("Testing Database insert_key with different values") {
+    Database* database = new Database();
+    database->insert_key("test_1", std::make_shared<Object>(10));
+    database->insert_key("test_1", std::make_shared<Object>("radu"));
+
+    const std::map<std::string, std::shared_ptr<Object>>& data =
+        database->get_data();
+
+    CHECK(data.find("test_1") != data.end());
+    CHECK(data.find("test_1")->second.get()->asString() == "radu");
+}
+
 TEST_CASE("Testing Database select") {
     Database* database = new Database();
     database->insert_key("test_1", std::make_shared<Object>(10));
@@ -23,6 +36,7 @@ TEST_CASE("Testing Database select") {
     CHECK(database->select("test_2").value().get()->asString() == "radu");
     CHECK(database->select("test_3").has_value() == false);
 }
+
 TEST_CASE("Testing Database delete_key") {
     Database* database = new Database();
     database->insert_key("test_1", std::make_shared<Object>(10));
